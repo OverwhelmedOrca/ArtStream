a1a3a44e7d27ffbb649ed8541bae8f6b5a3e1e06
OverwhelmedOrca <cdtpranavkumar1126@gmail.com>
2024-07-26 07:13:04 +0530

    all the changes

---
+++ hardhat/contracts/SimpleNFT.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SimpleNFT is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SimpleNFT", "SNFT") Ownable(msg.sender) {}
+
+    function safeMint(address to, string memory uri) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _safeMint(to, newTokenId);
+        _setTokenURI(newTokenId, uri);
+        return newTokenId;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-ethers");
 require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.19",
   networks: {
     theta_testnet: {
       url: "https://eth-rpc-api-testnet.thetatoken.org/rpc",
